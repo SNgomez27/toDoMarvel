05fd565d37aa8a4ece82d44c91f5d975f44714ce
Santiago Nicolas Gomez <gomezsantus@gmail.com>
2024-12-10 14:27:54 +0100

    Agregamos la funcionalidad para que se guarde el usuario que creamos en el register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {PopupService} from '../services/utils/popup.service';
 import {Router} from '@angular/router';
+import {RegisterService} from '../services/auth/register.service';
+import {NewUser} from '../services/interfaces/usuario';
 
 @Component({
   selector: 'app-register',
@@ -15,12 +17,14 @@ export class RegisterComponent {
   constructor(
     public formBuilder: FormBuilder,
     private popupService: PopupService,
-    private router: Router
+    private router: Router,
+    private registerService: RegisterService,
+
   ) {
     this.registerForm = formBuilder.group({
-      username: ['', [Validators.required,Validators.minLength(3)]],
-      name: ['', [Validators.required,Validators.minLength(3)]],
-      age: ['0'],
+      username: ['', [Validators.required,Validators.minLength(2)]],
+      nombre : ['', [Validators.required,Validators.minLength(2)]],
+      edad: ['0'],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
       confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
@@ -32,10 +36,28 @@ export class RegisterComponent {
   }
 
   enviar () {
-    if (this.registerForm.invalid) {
+    if (this.registerForm.invalid)
       return;
-    }
+    const  nuevoUsuario: NewUser ={
+      nombre : this.registerForm.value.name,
+      username: this.registerForm.value.username,
+      password: this.registerForm.value.password,
+      email: this.registerForm.value.email,
+      edad : this.registerForm.value.edad}
 
+
+      this.registerService.createUser(
+        this.registerForm.value as NewUser).subscribe({
+        next: ()=> {
+          this.popupService.showMessage(
+            "success",
+            "Registro exitoso!",
+            "te has registrado correctamente:")
+        },
+        error: error=> {
+          console.log(error);
+        }
+      })
 
     this.popupService.showMessage(
       "success", "Registrarse",
@@ -44,3 +66,4 @@ export class RegisterComponent {
     this.router.navigate(['login'])
   }
 }
+
