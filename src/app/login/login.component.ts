99a630d02a05b934a294048b0f0fd1e6723698d0
Santiago Nicolas Gomez <gomezsantus@gmail.com>
2024-12-12 14:11:27 +0100

    añadimos para que verifique si el usuario y la contra existe

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {PopupService} from '../services/utils/popup.service';
+import {LoginService} from '../services/auth/login.service';
+import {LoginUser} from '../services/interfaces/usuario';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -15,6 +18,7 @@ export class LoginComponent {
   constructor(
     private formBuilder: FormBuilder,
     private popupService: PopupService,
+    private loginService: LoginService
   ) {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
@@ -25,6 +29,33 @@ export class LoginComponent {
     this.cambiarVistaContra = !this.cambiarVistaContra;
   }
 enviar(): void {
+    this.loginService.loginv2(this.loginForm.value as LoginUser).subscribe({
+      next:data => {
+        this.popupService.showMessage(
+          "success",
+          "Iniciar sesion",
+          "se ha iniciado sesion correctamente"
+        )
+      },
+      error: err=> {
+        if (err.status === 401) {
+          this.popupService.showMessage(
+            "error",
+            "ops la contraseña es incorrecta",
+            "intentalo de nuevo"
+          )
+        } else {
+          this.popupService.showMessage(
+            "error",
+            "ops algo salio mal",
+            "intentalo de nuevo"
+          )
+
+        }
+      }
+    })
+
+
     if (this.loginForm.invalid){
       return;
 } else {
@@ -34,4 +65,9 @@ enviar(): void {
 
     }
   }
+
+
+
+
+
 }
